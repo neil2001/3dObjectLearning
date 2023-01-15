41218938dd9be7824260744cdc6b02eb29971443
neil2001 <52434451+neil2001@users.noreply.github.com>
2023-01-15 20:21:15 +0900

    visualizing confusion matrix

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 # 3dObjectLearning
+For the full writeup, visit this link: 
 
-# Epochs on Tensorflow Model with 3 classes
+# Epochs vs. Accuracy on Tensorflow Model with 3 simple objects (cone, sphere, cube)
 1 epoch: 87% training accuracy, 68% testing accuracy
+
 2 epochs: 95% training accuracy, 87% testing accuracy
+
 3 epochs: 98% training accuracy, 97% testing accuracy
+
 4 epochs: 99% training accuracy, 99% testing accuracy
+
+# Epochs vs. Accuracy on Tensorflow Model with 3 complex objects (cloud, snowman, ice cream cone)
+1 epoch: 84% training accuracy, 84% testing accuracy
+
+2 epochs: 92% training accuracy, 80% testing accuracy
+
+3 epochs: 94% training accuracy, 96% testing accuracy
+
+4 epochs: 97% training accuracy, 96% testing accuracy
+
+5 epochs: 97% training accuracy, 99% testing accuracy
+
+6 epochs: 99% training accuracy, 99% testing accuracy
 
--- main_tf.py
+++ main_tf.py
@@ -9,6 +9,9 @@ import tensorflow as tf
 import math
 # from tf import nn, optim
 
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+
 def getTrainTestValData(fileLoc, test_size = 0.15, val_size = 0.15):
     shapes, labels = preprocess_tf(fileLoc, 3)
     trainX = shapes[:math.floor(len(shapes)*(1-test_size-val_size))]
@@ -26,7 +29,7 @@ def getTrainTestValData(fileLoc, test_size = 0.15, val_size = 0.15):
     
 def testNN_tf():
     # test_size = 0.2
-    num_epoch = 6
+    num_epoch = 1
     BATCH_SIZE = 32
 
     primitiveFile = 'sphereConeCubeData.csv'
@@ -54,12 +57,39 @@ def testNN_tf():
     
     model.fit(train_dataset, epochs=num_epoch, validation_data=val_dataset)
 
+    snowModel = 'snowModel'
+    shapeModel = 'shapeModel'
+
+    model.save_weights(snowModel)
+
     preds = model.predict(test_dataset.batch(2))
 
     print(model.accuracy(preds, testY))
 
+def visualizeModel(snow=True):
+    modelName = 'snowModel' if snow else 'shapeModel'
+    # model = tf.keras.model.load_model(modelName)
+    model = PointNet_TF(num_classes=3)
+    load_status = model.load_weights(modelName)
+    load_status.expect_partial()
+    fileName = 'snowDataset.csv' if snow else 'sphereConeCubeData.csv'
+    _, _, _, _, testX, testY = getTrainTestValData(fileName, test_size = 0.5)
+    test_dataset = tf.data.Dataset.from_tensor_slices((testX, testY))
+
+    logits = model.predict(test_dataset.batch(2))
+    print("accuracy", model.accuracy(logits, testY).numpy())
+    preds = tf.argmax(logits, 1)
+    # print(preds)
+
+    confMat = confusion_matrix(testY, preds)
+    confMatPlot = sns.heatmap(confMat, annot=True)
+    figure = confMatPlot.get_figure()    
+    figure.savefig('conf_mat_' + modelName +'.png', dpi=400)
+
 def main():
-    testNN_tf()
+    # testNN_tf()
+
+    visualizeModel()
 
 if __name__ == "__main__":
     main()
